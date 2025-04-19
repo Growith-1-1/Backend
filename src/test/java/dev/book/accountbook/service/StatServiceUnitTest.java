09ea872d7b171530e177bfd785ad57ae744660d2
BHC-Chicken <tij05078@gmail.com>
2025-04-19 19:50:52 +0900

    [refactor] 테스트 코드 변경
    - 소비 내역 없으면 예외 반환에서 빈 객체 반환으로 변경에 따라 예외 테스트 제거

--- src/test/java/dev/book/accountbook/service/StatServiceUnitTest.java
+++ src/test/java/dev/book/accountbook/service/StatServiceUnitTest.java
@@ -4,7 +4,6 @@ import dev.book.accountbook.dto.response.AccountBookConsumeResponse;
 import dev.book.accountbook.dto.response.AccountBookSpendResponse;
 import dev.book.accountbook.dto.response.AccountBookStatResponse;
 import dev.book.accountbook.entity.AccountBook;
-import dev.book.accountbook.exception.accountbook.AccountBookErrorException;
 import dev.book.accountbook.repository.AccountBookRepository;
 import dev.book.accountbook.type.Category;
 import dev.book.accountbook.type.CategoryType;
@@ -17,14 +16,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -67,8 +64,8 @@ class StatServiceUnitTest {
         Category category = Category.HOBBY;
 
         List<AccountBook> mockBooks = List.of(
-                new AccountBook(1L, "볼링", category, CategoryType.SPEND, 15000, null, "memo1", user, null, null, null),
-                new AccountBook(2L, "영화", category, CategoryType.SPEND, 12000, null, "memo2", user, null, null, null)
+                new AccountBook("볼링", category, CategoryType.SPEND, 15000, null, "memo1", user, null, null, null),
+                new AccountBook("영화", category, CategoryType.SPEND, 12000, null, "memo2", user, null, null, null)
         );
 
         given(accountBookRepository.findByCategory(anyLong(), any(), any(), any(LocalDateTime.class), any(LocalDateTime.class))).willReturn(mockBooks);
@@ -81,7 +78,7 @@ class StatServiceUnitTest {
     }
 
     @Test
-    @DisplayName("주기별 절약금액을 가져온다..")
+    @DisplayName("주기별 절약금액을 가져온다.")
     void consume() {
         // given
         UserEntity user = mock(UserEntity.class);
@@ -97,39 +94,5 @@ class StatServiceUnitTest {
 
         // then
         assertThat(5000).isEqualTo(result.consume());
-    }
-
-    @Test
-    @DisplayName("해당 주기에 값이 없다면 예외를 반환한다.")
-    void getStatListFail() {
-        // given
-        UserEntity user = mock(UserEntity.class);
-        Frequency frequency = Frequency.MONTHLY;
-        LocalDateTime start = frequency.calcStartDate();
-        given(accountBookRepository.findTop3Categories(anyLong(), any(LocalDateTime.class), any(LocalDateTime.class), eq(PageRequest.of(0, 3))))
-                .willReturn(List.of());
-
-        // when
-        // then
-        assertThatThrownBy(() -> statService.statList(user.getId(), frequency))
-                .isInstanceOf(AccountBookErrorException.class)
-                .hasMessage("존재하지 않는 소비내역입니다.");
-    }
-
-    @Test
-    @DisplayName("해당 카테고리에 값이 없다면 예외를 반환한다.")
-    void getCategoryListFail() {
-        // given
-        UserEntity user = mock(UserEntity.class);
-        Frequency frequency = Frequency.MONTHLY;
-        LocalDateTime start = frequency.calcStartDate();
-        given(accountBookRepository.findByCategory(anyLong(), any(), any(), any(LocalDateTime.class), any(LocalDateTime.class)))
-                .willReturn(List.of());
-
-        // when
-        // then
-        assertThatThrownBy(() -> statService.categoryList(user.getId(), frequency, Category.HOBBY))
-                .isInstanceOf(AccountBookErrorException.class)
-                .hasMessage("존재하지 않는 소비내역입니다.");
     }
 }
